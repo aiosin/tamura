1fef5cbef8043622140d38bac7b1a1d22d79af6d
Wilhelm Buchmüller <buch.willi@googlemail.com>
2018-06-16 23:49:19 +0200

    coarseness step 2 -4
    not yet tested

--- tamura.py
+++ tamura.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 def checkarray(arr):
     #TODO: implement
     pass
@@ -11,7 +12,7 @@ def coarseness(arr):
     #check if array is legal ie two or three dimensions, non comple
     #TODO: implement three dimensional tamura features
     #also this assertion line could be improved
-    assert checkarray(arr)[0] == True
+    #assert checkarray(arr)[0] == True
     #if possible cast to int
     src_arr = np.array(arr,dtype='int')
 
@@ -19,25 +20,64 @@ def coarseness(arr):
     pad_size = 2**(k-1)
     
     #allocate padded source array and destination array for local avg calculations
+    #note that the value doubled here is not the padding size but the tuple
     pad_arr = np.pad(src_arr,(2*(pad_size,) ),'constant')
-    l_avg_arr = np.zeros((src_arr.shape)+(5,),dtype='int')
-
+    l_avg_arr = np.zeros((src_arr.shape)+(k,),dtype='int')
+    pad_l_avg_arr = np.pad(l_avg_arr,(2*(pad_size,)),'constant')
     #step 1: calculate the averages over the neighborhood of size 2^k x 2^k
     rows,cols = src_arr.shape
     for i in range(0,rows):
         for j in range(0,cols):
             tmp = np.zeros((k,),dtype='int')
-            for m in range(0,k):
-                tmp_arr = pad_arr[pad_size+i-(2**(k-1)):pad_size+i+(2**(k-1)-1),pad_size+j-(2**(k-1)):pad_size+j+(2**(k-1)-1)]
+            #TODO: check correctness of index shift (1,k+1)
+            for m in range(1,k+1):
+                tmp_arr = pad_arr[pad_size+i-(2**(m-1)):pad_size+i+(2**(m-1)-1),
+                                pad_size+j-(2**(m-1)):pad_size+j+(2**(m-1)-1)]
                 acc_sum = np.sum(tmp_arr)
-                # reminder that 2^k * 2^k = 2^(2k)
-                tmp[m] = acc_sum / 2**(2*k)
+                # reminder that 2^m * 2^m = 2^(2m)
+                tmp[m] = acc_sum / 2**(2*m)
             l_avg_arr[i][j] = tmp
 
     #l_avg_arr contains now the averages 
-
+    E_h = np.zeros((src_arr.shape)+(k,),dtype='int')
+    E_v = np.zeros((src_arr.shape)+(k,),dtype='int')
 
     #step 2:
+    for i in range(0,rows):
+        for j in range(0,cols):
+            #index shift: 
+            for m in range(1,k+1):
+                E_h[i,j,m] = abs(pad_l_avg_arr[i,j+(2**(m-1)),m] -
+                                pad_l_avg_arr[i,j-(2**(m-1)),m])
+                E_v[i,j,m] = abs(pad_l_avg_arr[i+(2**(m-1)),j,m] -
+                                pad_l_avg_arr[i-(2**(m-1)),j,m])
+    s_arr = np.zeros((src_arr.shape),dtype='int')
+    #step 3:
+    for i in range(0,rows):
+        for j in range(0,cols):
+            s_best = None
+            h_max_v = np.max(E_h[i,j])
+            v_max_v = np.max(E_v[i,j])
+            h_max_i = np.argmax(E_h[i,j])
+            v_max_i = np.argmax(E_v[i,j])
+
+            if h_max_v < v_max_v:
+                s_best = 2**v_max_v
+            else:
+                s_best = 2**h_max_v
+                
+            s_arr[i,j] = s_best
+    
+    #step 4:
+    avg = np.mean(s_arr)
+    return avg
 
 def tamura_3D(arr):
     pass
+
+
+def main():
+    coarseness(np.array(np.arange(20).reshape((4,5))))
+
+if __name__ == '__main__':
+    main()
